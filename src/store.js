5079c7b4b8dc9c28fbbb7d1e00e14ae063e0e7dc
federicogramajo <federico.gramajo.medina@gmail.com>
2019-11-21 15:20:10 -0300

    cambio heroku

--- src/store.js
+++ src/store.js
@@ -74,7 +74,7 @@ export const handleLogin = (email, password) => {
     })
 
     const options = {
-      baseURL: 'https://trabajo-mcga-gramajo.herokuapp.com/',
+      baseURL: 'https://login-mcga-gramajo.herokuapp.com/',
       timeout: 25000,
       method: 'POST',
       headers: {
@@ -82,7 +82,7 @@ export const handleLogin = (email, password) => {
       },
     }
 
-    return fetch(`https://trabajo-mcga-gramajo.herokuapp.com/login`, { ...options, body: JSON.stringify({ email, password }) })
+    return fetch(`https://login-mcga-gramajo.herokuapp.com/login`, { ...options, body: JSON.stringify({ email, password }) })
       .then(res => res.json())
       .then(data => {
         if (!data.success) {
